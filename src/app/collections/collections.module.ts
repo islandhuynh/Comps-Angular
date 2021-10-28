244d5ab0c794c86d3a07dc903d0096caa451bbef
Island <islandhuynh@gmail.com>
2021-10-28 04:28:58 -0400

    Added a placeholder component

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { TimesDirective } from './times.directive';
 
 
 @NgModule({
   declarations: [
-    ElementsHomeComponent
+    ElementsHomeComponent,
+    PlaceholderComponent,
+    TimesDirective
   ],
   imports: [
     CommonModule,
