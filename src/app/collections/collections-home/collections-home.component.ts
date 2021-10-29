58aef6a32ccaff565d0a43f3dbe889ec090ab9eb
Island <islandhuynh@gmail.com>
2021-10-28 20:13:17 -0400

    added component to generate table based on specified data and takes in class names from parent template

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 24, job: 'Engineer', employed: true},
+    { name: 'Jill', age: 26, job: 'Engineer', employed: false},
+    { name: 'Elyse', age: 25, job: 'Designer', employed: true}
+  ];
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+    { key: 'employed', label: 'Employed'}
+  ];
 
   constructor() { }
 
